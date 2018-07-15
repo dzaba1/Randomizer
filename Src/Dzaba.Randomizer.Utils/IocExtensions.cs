2fddf53652ce312e8d67d575888123027f9e0b8b
Dzaba <blazej.zabialowicz@gmail.com>
2018-07-15 15:35:09 +0200

    Added utils proj

---
+++ Src/Dzaba.Randomizer.Utils/IocExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Ninject;
+using System;
+
+namespace Dzaba.Randomizer.Utils
+{
+    public static class IocExtensions
+    {
+        public static void RegisterTransient<TInt, TImpl>(this IKernel container)
+            where TImpl : TInt
+        {
+            Require.NotNull(container, nameof(container));
+
+            container.Bind<TInt>().To<TImpl>().InTransientScope();
+        }
+
+        public static void RegisterSingleton<TInt, TImpl>(this IKernel container)
+            where TImpl : TInt
+        {
+            Require.NotNull(container, nameof(container));
+
+            container.Bind<TInt>().To<TImpl>().InSingletonScope();
+        }
+
+        public static void RegisterFactoryMethod<T>(this IKernel container)
+        {
+            Require.NotNull(container, nameof(container));
+
+            container.Bind<Func<T>>()
+                .ToMethod(c => () => c.Kernel.Get<T>())
+                .InTransientScope();
+        }
+    }
+}
---
+++ Src/Dzaba.Randomizer.Utils/Require.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dzaba.Randomizer.Utils
+{
+    public static class Require
+    {
+        public static void NotNull(object obj, string argumentName)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+
+        public static void NotEmpty(string str, string argumentName)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+
+        public static void NotEmpty<T>(IEnumerable<T> collection, string argumentName)
+        {
+            NotNull(collection, argumentName);
+
+            if (!collection.Any())
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+
+        public static void NotWhiteSpace(string str, string argumentName)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+
+        public static void IndexInRange(int current, int length, string argumentName)
+        {
+            if (current < 0 || current >= length)
+            {
+                throw new ArgumentOutOfRangeException(argumentName, $"Variable '{argumentName}' is out of range of length {length}.");
+            }
+        }
+
+        public static void InRange(double current, double min, double max, string argumentName)
+        {
+            if (current < min || current > max)
+            {
+                throw new ArgumentOutOfRangeException(argumentName, $"Variable '{argumentName}' is out of range (${min}, ${max}).");
+            }
+        }
+    }
+}
