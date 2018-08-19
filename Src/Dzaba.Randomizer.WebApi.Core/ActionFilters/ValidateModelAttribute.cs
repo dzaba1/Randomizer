5eda959b88293976453de368933381fdc04cb308
Dzaba <blazej.zabialowicz@gmail.com>
2018-08-19 12:12:27 +0200

    Created valide model attribute

---
+++ Src/Dzaba.Randomizer.WebApi.Core/ActionFilters/ValidateModelAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Dzaba.Randomizer.WebApi.Core.ActionFilters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    sealed class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}
