18b0de8ecafbcb37198ab5fe2b97d42f563e09e2
Dzaba <blazej.zabialowicz@gmail.com>
2018-08-19 14:28:52 +0200

    Fixing DI

--- Src/Dzaba.Randomizer.WebApi.Core/Startup.cs
+++ Src/Dzaba.Randomizer.WebApi.Core/Startup.cs
@@ -3,23 +3,33 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Dzaba.Randomizer.WebApi.Core
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Require.NotNull(configuration, nameof(configuration));
+
+            this.configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             var container = new Containers(services);
+
+            container.Kernel.Bind<IConfiguration>()
+                .ToConstant(configuration)
+                .InSingletonScope();
+
             container.RegisterAll();
+
+            return container.Kernel;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
